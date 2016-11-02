231ec886bcb8319e81e8aca11d8386a616b78dea
zhangwen <i.am.zw129@gmail.com>
2016-11-02 16:35:19 +0800

    new theme

---
+++ pelican_blog/pelican/copy_image.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+import pelicanconf
+import os
+import shutil
+
+image_extends = [".png", ".gif", ".jpg", ".jpeg"]
+
+def getAllImageFile(dirPath):
+    ret = []
+    allsub = os.listdir(dirPath)
+    for sub in allsub:
+        subPath = "%s/%s" % (dirPath, sub)
+        if os.path.isfile(subPath):
+        	   for ext in image_extends:
+        	       if subPath.endswith(ext):
+        	           ret.append(subPath)
+        	           break
+
+        if os.path.isdir(subPath):
+            ret.extend(getAllImageFile(subPath))
+    return ret
+
+if __name__ == '__main__':
+	images = getAllImageFile(pelicanconf.PATH)
+	for image in images:
+		imageName = os.path.basename(image)
+		shutil.copy(image, "%s/%s" % (pelicanconf.OUTPUT_PATH, imageName))
